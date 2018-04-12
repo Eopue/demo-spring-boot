bf7195d01f30c9d530351f2bac9c80551a03adb0
liuxiaolu <liuxiaolu@cloud-star.com.cn>
2018-04-12 17:09:03 +0800

    fix 构建war包访问不到

--- src/main/java/cn/com/demo/DemoApplication.java
+++ src/main/java/cn/com/demo/DemoApplication.java
@@ -3,6 +3,8 @@ package cn.com.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -17,9 +19,16 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement
 @MapperScan(basePackages = {"cn.com.demo.dao"})
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // TODO Auto-generated method stub
+        builder.sources(this.getClass());
+        return super.configure(builder);
     }
 }
